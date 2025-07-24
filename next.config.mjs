094db024eaca50e8bff28cce96717fd4e7b16525
Rawier <alexam211@hotmail.com>
2025-07-24 13:56:28 -0600

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
   generateEtags: true,
   trailingSlash: false,
   reactStrictMode: true,
-  swcMinify: true,
+
   experimental: {
     optimizeCss: true,
     optimizePackageImports: ["lucide-react", "@radix-ui/react-icons"],
