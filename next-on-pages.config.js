cc0f73cdd1ca7be114ab85207e5fbb1d46c93e32
Rawier <alexam211@hotmail.com>
2025-07-24 13:09:55 -0600

    fix: deploy

--- next-on-pages.config.js
+++ next-on-pages.config.js
@@ -1,14 +1,5 @@
 /** @type {import('@cloudflare/next-on-pages').Config} */
 const config = {
-  // Configuración específica para Cloudflare Pages
-  experimental: {
-    runtime: 'edge',
-  },
-  // Forzar Edge Runtime para todas las rutas de API
-  edgeRuntime: {
-    '/api/admin/*': true,
-    '/api/*': true,
-  },
   // Configuración de compatibilidad
   compatibility: {
     nodejs: false,
