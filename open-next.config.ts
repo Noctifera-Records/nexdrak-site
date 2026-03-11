82e61948536c1b5542f8bc0b3472bd6e786b74e1
aiskoa <alexam211@hotmail.com>
2026-03-11 01:34:20 -0600

    fix: deploy

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,7 +2,7 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
   build: {
-    minify: true,
+    minify: false,
     // Exclude pg-native from build (it's optional and fails in Workers)
     external: ["pg-native"],
   },
